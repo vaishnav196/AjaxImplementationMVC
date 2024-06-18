3b4fede6aab38952c9af7dc42dc7a9cdc725c74f
vaishnav196 <vaish00721@gmail.com>
2024-06-18 23:49:10 +0530

    ..

--- AjaxImplementationMVC/Controllers/ProductsController.cs
+++ AjaxImplementationMVC/Controllers/ProductsController.cs
@@ -38,5 +38,52 @@ namespace AjaxImplementationMVC.Controllers
             return new JsonResult(data);
 
         }
+        public IActionResult SearchProducts(string searchp)
+        {
+            if (string.IsNullOrEmpty(searchp))
+            {
+                var data= db.products.ToList();
+                return new JsonResult(data);
+            }
+            else
+            {
+                var data=db.products.Where(x=>x.Pname.Contains(searchp) || x.Pcat.Contains(searchp) || x.Price.ToString().Contains(searchp)).ToList();   
+                return new JsonResult(data);
+            }
+
+        }
+
+
+        public IActionResult DeleteProducts(int prodid)
+        {
+            var data = db.products.Find(prodid);
+            if (data!= null)
+            {
+                db.products.Remove(data);
+                db.SaveChanges();
+
+            }
+            return new JsonResult("");
+
+        }
+
+
+
+        public IActionResult UpdateProducts(int prodid)
+        {
+            var data = db.products.Find(prodid);
+           
+            return new JsonResult(data);
+
+        }
+
+     
+        public IActionResult ModifyProducts(Product p)
+        {
+            db.products.Update(p);
+            db.SaveChanges();   
+            return new JsonResult("");
+
+        }
     }
 }
--- AjaxImplementationMVC/wwwroot/js/JavaScript.js
+++ AjaxImplementationMVC/wwwroot/js/JavaScript.js
@@ -1,9 +1,14 @@
 ﻿$(document).ready(function () {
     showProducts();
+   
 });
 
 $("#btn").click(function () {
-    $("#mymodal").modal('show')
+    $("#mymodal").modal('show');
+    $("#savebtn").show();
+    $("#updbtn").hide();
+    $("#exampleModalLabel").text("Add New Products");
+    $("#id").hide();
 });
 
 $("#close").click(function () {
@@ -20,7 +25,12 @@ $("#savebtn").click(function () {
         contentType: 'Application/X-www-form-urlencoded; charset=utf-8;',
         data: obj,
         success: function () {
+         
             alert("Product Added Succesfully");
+            showProducts();
+            $("#mymodal").modal('hide');
+            clear();
+
            
         },
 
@@ -47,6 +57,7 @@ function showProducts() {
                 obj += "<td>" + item.pname + "</td>";
                 obj += "<td>" + item.pcat + "</td>";
                 obj += "<td>" + item.price + "</td>";
+                obj += "<td><a onclick='RemoveProducts(" + item.pid + ")' class='btn btn-danger btn-sm'>Delete</a> <a  onclick='EditProducts("+item.pid+")' class='btn btn-primary btn-sm mx-2'>Edit</a></td>";
                 obj += "</tr>";
             })
             $("#tdata").html(obj);
@@ -56,4 +67,122 @@ function showProducts() {
             alert("Something Errored happend")
         }
     })
+}
+
+
+
+function RemoveProducts(id) {
+
+    if (confirm("Are you sure you Want to delete this record")) {
+        $.ajax({
+            url: '/Products/DeleteProducts?prodid=' + id,
+
+            success: function () {
+
+                alert("Product Delete Succesfully");
+                showProducts();
+            },
+
+            Error: function () {
+                alert("Something Errored happend");
+            }
+        })
+    }
+    else {
+        alert("Record Not Deleted");
+    }
+
+  
+}
+
+
+function EditProducts(id) {
+    $.ajax({
+        url:'/Products/UpdateProducts?prodid=' + id,
+
+        success: function (response) {
+
+            $("#mymodal").modal('show');
+            $("#Pid").val(response.pid);
+            $("#Pname").val(response.pname);
+            $("#Pcat").val(response.pcat);
+            $("#Price").val(response.price);
+
+            $("#savebtn").hide();
+            $("#updbtn").show();
+            $("#exampleModalLabel").text("Edit Products");
+            $("#id").show();
+            showProducts();
+        },
+
+        Error: function () {
+            alert("Something Errored happend");
+        }
+    })
+}
+
+
+
+$("#updbtn").click(function () {
+    var obj = $("#myform").serialize();
+    $.ajax({
+        url: '/Products/ModifyProducts',
+        type: 'Post',
+        dataType: 'json',
+        contentType: 'Application/X-www-form-urlencoded; charset=utf-8;',
+        data: obj,
+        success: function () {
+
+            alert("Product Updated Succesfully");
+            showProducts();
+            $("#mymodal").modal('hide');
+            clear();
+
+
+        },
+
+        Error: function () {
+            alert("Something Errored happend")
+        }
+
+    });
+});
+
+
+
+
+$("#srchbtn").click(function () {
+    var search = $("#srch").val();
+    $.ajax({
+        url: '/Products/SearchProducts?searchp='+search,
+        type: 'Post',
+        dataType: 'json',
+        contentType: 'Application/json;charset=utf-8;',
+        success: function (result, sta, xhr) {
+            var obj = '';
+            $.each(result, function (index, item) {
+                obj += "<tr>";
+                obj += "<td>" + item.pid + "</td>";
+                obj += "<td>" + item.pname + "</td>";
+                obj += "<td>" + item.pcat + "</td>";
+                obj += "<td>" + item.price + "</td>";
+                obj += "<td><a onclick='RemoveProducts(" + item.pid + ")' class='btn btn-danger btn-sm'>Delete</a> <a  onclick='EditProducts(" + item.pid + ")' class='btn btn-primary btn-sm mx-2'>Edit</a></td>";
+                obj += "</tr>";
+            })
+            $("#tdata").html(obj);
+        },
+
+        Error: function () {
+            alert("Something Errored happend")
+        }
+    })
+
+});
+
+
+
+function clear() {
+    $("#Pname").val('');
+    $("#Pcat").val('');
+    $("#Price").val('');
 }
